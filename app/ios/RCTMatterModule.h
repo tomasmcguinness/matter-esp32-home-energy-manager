5ed2cbcbeb47eb3d7ff426b3e99d94b45193ab8f
Tomas McGuinness <tomas@tomasmcguinness.com>
2026-04-30 19:47:05 +0100

    Discard ReactNative

--- app/ios/RCTMatterModule.h
+++
@@ -1,3 +0,0 @@
-#import <React/RCTBridgeModule.h>
-@interface RCTMatterModule : NSObject <RCTBridgeModule>
-@end
--- app/ios/RCTMattermodule.m
+++
@@ -1,7 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(MatterModule, NSObject)
-
-RCT_EXTERN_METHOD(commissionDevice)
-
-@end
