f5d33af61ec3b816fe9d385074a4eeeaaabc7aa3
Tomas McGuinness <tomas@tomasmcguinness.com>
2026-05-12 19:34:03 +0100

    Add ability to the companion app to enter and save a new device's name

--- app/RequestHandler/ESP32Client.swift
+++ app/RequestHandler/ESP32Client.swift
@@ -21,7 +21,7 @@ class ESP32Client {
         self.baseURL = baseURL
     }
 
-    func commissionDevice(onboardingPayload: String) async throws {
+    func commissionDevice(onboardingPayload: String) async throws -> UInt64 {
         var request = URLRequest(url: baseURL.appendingPathComponent("controller/commission"))
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -29,6 +29,25 @@ class ESP32Client {
         request.httpBody = try JSONSerialization.data(withJSONObject: [
             "onboardingPayload": onboardingPayload
         ])
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let http = response as? HTTPURLResponse, http.statusCode == 200 else {
+            let code = (response as? HTTPURLResponse)?.statusCode ?? 0
+            throw ESP32ClientError.commissionFailed(code)
+        }
+        let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        guard let nodeId = json?["nodeId"] as? Double else {
+            throw ESP32ClientError.invalidResponse
+        }
+        return UInt64(nodeId)
+    }
+
+    func setDeviceName(nodeId: UInt64, name: String) async throws {
+        let url = baseURL.appendingPathComponent("api/devices/\(nodeId)/name")
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try JSONSerialization.data(withJSONObject: ["name": name])
 
         let (_, response) = try await URLSession.shared.data(for: request)
         guard let http = response as? HTTPURLResponse, http.statusCode == 200 else {
